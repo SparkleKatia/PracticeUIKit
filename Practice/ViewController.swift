aebd7f65214aa56885573e4b59235d5447859707
Kateryna Zhyrnova <kateryna.zhyrnova.sparkle@gmail.com>
2025-07-30 14:59:18 +0200

    Initial Commit

---
+++ Practice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Practice
+//
+//  Created by Kateryna on 30/07/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
